03b2dbbce78bbe40608baab3e6f22f63b495258e
sashok58 <sashok58@gmail.com>
2016-05-12 17:30:43 +0300

    Default commit

--- Chapter4/src/com/homework/Text.java
+++ Chapter4/src/com/homework/Text.java
@@ -3,5 +3,12 @@ package com.homework;
 
 public class Text extends Sentence{
 
+public void addText (String text){
+
+}
+    public void printTextToConsole(String text){
+        System.out.println(text);
+    }
+
 
 }
