cd46ad2cc494c1e138fab81f369004f4e4830a7a
sashok58 <sashok58@gmail.com>
2016-05-12 14:15:19 +0300

    Default commit

---
+++ Chapter4/src/com/homework/Sentence.java
@@ -0,0 +1,14 @@
+package com.homework;
+
+
+public class Sentence extends Word{
+protected String sentence;
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+}
---
+++ Chapter4/src/com/homework/Word.java
@@ -0,0 +1,14 @@
+package com.homework;
+
+
+public class Word {
+    protected String word;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
